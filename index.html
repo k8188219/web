f228fe515bf5826117224bcee52862d8c975790b
k8188219 <k8188219@mail.edu.tw>
2017-11-10 02:52:05 +0800

    Add files via upload

---
+++ index.html
@@ -0,0 +1,33 @@
+<html>
+
+<head>
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width, user-scalable=no">
+  <link href="http://jqmdesigner.appspot.com/gk/lib/jquery.mobile/1.4.5/jquery.mobile-1.4.5.min.css" rel="stylesheet" type="text/css" />
+  <!-- Export CSS  -->
+  <!-- Uncomment the following to include cordova in your android project -->
+  <!--<script src="http://jqmdesigner.appspot.com/platforms/android/cordova.js"></script>-->
+  <!-- Export JS  -->
+  
+  <title>PigChat</title>
+</head>
+
+<body>
+  <!-- Page: home  -->
+  <style>
+  iframe { 
+      height:382px;
+      width:510px; 
+    }
+  @media screen and (max-width: 510px) {
+    iframe { 
+      width:100%;
+    }
+  }
+  </style>
+  <div align="center">
+    <iframe src='playlist/AP.html' style='overflow:hidden;border: 0;margin:0;background: white;'></iframe>
+  </div>
+</body>
+
+</html>
---
+++ list.js
@@ -0,0 +1,222 @@
+var music = 
+[{
+	title: '13 私へ.mp3',
+	author: '',
+	url: 'https://drive.google.com/uc?id=166QC3m3MJbDMRgutyrdk7B1sSQD6kAwq',
+},{
+	title: '11 夜が明けるよ.mp3',
+	author: '',
+	url: 'https://drive.google.com/uc?id=1a44jAKsax64EuLFBWDht2LTTta_5Q7k8',
+},{
+	title: '09 星が瞬くこんな夜に.mp3',
+	author: '',
+	url: 'https://drive.google.com/uc?id=1yhIsix77gszXSzQOZZpCAR5qP8O2wAU-',
+},{
+	title: '12 さよならメモリーズ.mp3',
+	author: '',
+	url: 'https://drive.google.com/uc?id=1Uh_br_cfGuKM6LhGa0SpKhmDiphVNUYw',
+},{
+	title: '10 うたかた花火.mp3',
+	author: '',
+	url: 'https://drive.google.com/uc?id=17kuHInY42G6a2nVurw0kIp-qSO9nSiD0',
+},{
+	title: '03 ヒーロー.mp3',
+	author: '',
+	url: 'https://drive.google.com/uc?id=1UNut07tzu1TNQzLhn70rVRtu-8KBTH-c',
+},{
+	title: '05 復讐.mp3',
+	author: '',
+	url: 'https://drive.google.com/uc?id=1qvdh6BsAB8xVpGIsGBXitgUjNckPn0q8',
+},{
+	title: '08 Feel so good.mp3',
+	author: '',
+	url: 'https://drive.google.com/uc?id=1EseK80OPia4FethWsNNr8lvKkhTBrpXV',
+},{
+	title: '07 LOVE & ROLL.mp3',
+	author: '',
+	url: 'https://drive.google.com/uc?id=1fkKqeOqxaFB0-HEHXI-nafiThMN4HzQG',
+},{
+	title: '04 Perfect Day.mp3',
+	author: '',
+	url: 'https://drive.google.com/uc?id=1Ei7XXTgNyyv3HEpxb6xedFibFnjiAr3i',
+},{
+	title: '06 ロックンロールなんですの.mp3',
+	author: '',
+	url: 'https://drive.google.com/uc?id=1RrkUy4vCN4RFE0i6p0f36oMnxSLt0d1E',
+},{
+	title: '02 君の知らない物語.mp3',
+	author: '',
+	url: 'https://drive.google.com/uc?id=13fF9x21FgdnS72_F5L3PN6KcN30GM2iE',
+},{
+	title: '01 終わりへ向かう始まりの歌.mp3',
+	author: '',
+	url: 'https://drive.google.com/uc?id=1eCK_by3mWccYpVDbfen0qNeN-8x5EvDW',
+},{
+	title: '07 Sacramentum- Unaccompanied Hymn for Torino.wav',
+	author: '',
+	url: 'https://drive.google.com/uc?id=1p6345MjZRrY1J8GeLH-AZ0kzs4_foRyL',
+},{
+	title: "13 Witch's Invitation.wav",
+	author: '',
+	url: 'https://drive.google.com/uc?id=1yyZgvU3QNKKSIZxom-s4juY8i3hVHHxV',
+},{
+	title: '12 Maroma Samsa.wav',
+	author: '',
+	url: 'https://drive.google.com/uc?id=1NwPf18abM1GCIYC720Y8fK5K86FgsX0S',
+},{
+	title: '11 Rosetta.wav',
+	author: '',
+	url: 'https://drive.google.com/uc?id=1ht7XRez_Y9fVaHn2f5qPLu1v8DUNDRVP',
+},{
+	title: '03 Utopiosphere.wav',
+	author: '',
+	url: 'https://drive.google.com/uc?id=1yJsrX7sI6hfCokg16uf1Lwz3yArd9web',
+},{
+	title: '06 YUBIKIRI-GENMAN.wav',
+	author: '',
+	url: 'https://drive.google.com/uc?id=130K0m0YEl0ov7URTWWV-gIkDWVgIxoL0',
+},{
+	title: '08 Ephemeral.wav',
+	author: '',
+	url: 'https://drive.google.com/uc?id=1kCx_puVaJq9UdJ48MUyj9cU_yix9MLAj',
+},{
+	title: "01 A Turtle's Heart.wav",
+	author: '',
+	url: 'https://drive.google.com/uc?id=1SE3TnBYCjtnKc2Exijmv74bUAuprTNFQ',
+},{
+	title: '10 Fable.wav',
+	author: '',
+	url: 'https://drive.google.com/uc?id=1gpvCk9Ld6e2VO8JV5hzTWhhYueMBNns9',
+},{
+	title: '03. delete.flac',
+	author: '',
+	url: 'https://drive.google.com/uc?id=1-HnLGKPYsOORw29ZPt1tvoToJRyJ7jIb',
+},{
+	title: '09 Imagined Flight.wav',
+	author: '',
+	url: 'https://drive.google.com/uc?id=1-B6RHYZ8Yb5-VKXeqRT4GtWNBY2vxsV8',
+},{
+	title: '04. Break Beat Bark!.flac',
+	author: '',
+	url: 'https://drive.google.com/uc?id=1xAMRpmOzR9u8zg99mUguS4vXaApuIL6e',
+},{
+	title: '02 Nine Point Eight.wav',
+	author: '',
+	url: 'https://drive.google.com/uc?id=1nYjNoXMVHXkyA9V7hs_1sI11HVoWVUR1',
+},{
+	title: '05 Chocological.wav',
+	author: '',
+	url: 'https://drive.google.com/uc?id=1XqNUJXVv7xH-118IFiTl7fyCjcberzFK',
+},{
+	title: '05. smile for you.flac',
+	author: '',
+	url: 'https://drive.google.com/uc?id=1cejjeoB3LQMAsv6h7FygBQ-pFhmq8Rxa',
+},{
+	title: '01. Ubiquitous dB.flac',
+	author: '',
+	url: 'https://drive.google.com/uc?id=1Y93fikfQlB9_aBhMXiPFNuejhwy6uzBB',
+},{
+	title: '04 Friction.wav',
+	author: '',
+	url: 'https://drive.google.com/uc?id=1BStUs1Mg3vdzapCTgLTyUOPXH2iL4E6b',
+},{
+	title: '02. longing.flac',
+	author: '',
+	url: 'https://drive.google.com/uc?id=1KQw8oCCwcw4zq640OffimH5Ut_tvW6R0',
+},{
+	title: '06 Chen-U - Puppet.mp3',
+	author: '',
+	url: 'https://drive.google.com/uc?id=1Yw3DLJYAzJEjYFTRGPhNVzjjohf-Yce-',
+},{
+	title: '03 Ark Brown - Starlight Dance Floor.mp3',
+	author: '',
+	url: 'https://drive.google.com/uc?id=1GTR4DyuMmTPzNrRFmfOMctmStVTW4wDn',
+},{
+	title: '07 舞花 - Two of us.mp3',
+	author: '',
+	url: 'https://drive.google.com/uc?id=1F2RMKZZXdYASWOFQc1QSHcfk1wzwlmzK',
+},{
+	title: '02 Chen-U - Origin of Love.mp3',
+	author: '',
+	url: 'https://drive.google.com/uc?id=1WyEkfZVkcPurhGcS2UjgnC8wvTH6g2Gx',
+},{
+	title: '05 Maurits禅Cornelis - Faraway Sky.mp3',
+	author: '',
+	url: 'https://drive.google.com/uc?id=1bGC2SdR3qFaqwPvCvkDT6V47wNILwa75',
+},{
+	title: '04 Vivienne - Dazzling You.mp3',
+	author: '',
+	url: 'https://drive.google.com/uc?id=1Uw_EqZ-y3cpk0IfgB3glERpqnxr_Jy0f',
+},{
+	title: '01 Tim Vegas - Guardian ─Theme of TCIS─.mp3',
+	author: '',
+	url: 'https://drive.google.com/uc?id=1iPTjbHU9DlGTsjemuWglvrhwRbACSFhN',
+},{
+	title: '11 Ｌａｓｔ　Ｓｍｉｌｅ.mp3',
+	author: '',
+	url: 'https://drive.google.com/uc?id=1nT-m2e6yNKPfJl_3ijE5uMydcyLURAhI',
+},{
+	title: '12 Ｈｅｒｏの条件.mp3',
+	author: '',
+	url: 'https://drive.google.com/uc?id=1dlGKJw9ERapCZpCji_NeMMsRB3_MBHdd',
+},{
+	title: '09 雪の降らない星.mp3',
+	author: '',
+	url: 'https://drive.google.com/uc?id=1z2HBTAvpslWdFDWZppIyfgVur8Uun7lw',
+},{
+	title: '01 終わりの世界から.mp3',
+	author: '',
+	url: 'https://drive.google.com/uc?id=19pbKY0FO9msV0Ni-qOrOwle9V-W-HuYo',
+},{
+	title: '02 ふたりだけのＡｒｋ.mp3',
+	author: '',
+	url: 'https://drive.google.com/uc?id=16ZUNEEMMbwTHPLk8kWdcwfWKzwSK9U0o',
+},{
+	title: '03 Ｋｉｌｌｅｒ　Ｓｏｎｇ.mp3',
+	author: '',
+	url: 'https://drive.google.com/uc?id=1jnJOpeZNcMCGleQ2rRwjPEPftlfKsDjZ',
+},{
+	title: '06 凍る夢.mp3',
+	author: '',
+	url: 'https://drive.google.com/uc?id=1UO6zmjmYKZ4pqoTemiSfMhbaXIxesfyS',
+},{
+	title: '08 とある海賊王の気まぐれ.mp3',
+	author: '',
+	url: 'https://drive.google.com/uc?id=1N_o3KgnyaLh1XtlMH6Mr0xAaCSvkSZHk',
+},{
+	title: '10 火吹き山の魔法使い.mp3',
+	author: '',
+	url: 'https://drive.google.com/uc?id=16vP23n0tzGqIpPDrRthARZv7vJrLZJhd',
+},{
+	title: '05 無敵のＳｏｌｄｉｅｒ.mp3',
+	author: '',
+	url: 'https://drive.google.com/uc?id=1QzoJ7rftBZFv1gbjOspp9z_lvamn9BmD',
+},{
+	title: '07 Ｅｘｅｃｕｔｉｏｎｅｒの恋.mp3',
+	author: '',
+	url: 'https://drive.google.com/uc?id=1S17QHGKICr1yD7RDmtt5a0xbgcpnRkKE',
+},{
+	title: '04 Ｆｌｏｗｅｒ　Ｇａｒｄｅｎ.mp3',
+	author: '',
+	url: 'https://drive.google.com/uc?id=1LcXblC1xh8Obg5OmDFK3HTZt2K274oKs',
+},{
+	title: '13 この惑星（ホシ）のＢｉｒｔｈｄａｙ　Ｓｏｎｇ.mp3',
+	author: '',
+	url: 'https://drive.google.com/uc?id=1pXMv2Mwcm67v8au77cnz-F3IAOUz5Hlx',
+},{
+	title: '14 曲目 14.mp3',
+	author: '',
+	url: 'https://drive.google.com/uc?id=12FUSmlfptmYA-6LF_I60YH6zj9M-8hKD',
+},{
+	title: '01 恋愛サーキュレーション.flac',
+	author: '',
+	url: 'https://drive.google.com/uc?id=1AL5egYyKAvFYUkPotFlv2Kmv8cpw8SMf',
+},{
+	title: 'ClariS - ヒトリゴト.mp3',
+	author: '',
+	url: 'https://drive.google.com/uc?id=1cNC6NVKdp-WUzIdw69hHXFf9ZTYNY-RB',
+},{
+	title: 'BilibiliJJ.COM-【温暖女声】米白Alice（Cover：古川本舗）_(Av10911610，P1).mp3',
+	author: '',
+	url: 'https://drive.google.com/uc?id=1GCN0YAD4LBPqoxruiE2i0z3JD5KO1kum',
+}]
