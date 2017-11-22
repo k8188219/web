055570b8d3de9ba72733c6ccffa4bfc535775cff
k8188219 <k8188219@mail.edu.tw>
2017-11-22 21:48:53 +0800

    Add files via upload

---
+++ remote/remote.php
@@ -0,0 +1,73 @@
+﻿<?php
+ini_set('max_execution_time', 1200);#&#35373;&#32622;php&#22519;&#34892;&#26368;&#22823;&#26178;&#38291;(&#31186;)
+
+#&#20597;&#28204;&#36664;&#20837;
+if(!$_GET["f"]){
+    echo "<script>function myprint(){document.getElementsByTagName('p')[0].innerHTML ='http://pigchat.pe.hu/index.php?f='+ document.getElementsByTagName('input')[0].value}</script>";
+    echo "<input></input> ";
+    echo "<button onclick='myprint()'>button</button>";
+    echo "<p></p>";
+    exit();
+}else{
+    $url = $_GET["f"];
+}
+$size = 0;#&#25972;&#20491;&#27284;&#26696;&#22823;&#23567;
+$record = 0;#&#26368;&#21021;&#21462;&#24471;&#30070;&#26696;&#22823;&#23567;&#26041;&#27861;&#30340;&#21443;&#25976;
+$count = 0;#&#26368;&#21021;&#21462;&#24471;&#30070;&#26696;&#22823;&#23567;&#26041;&#27861;&#30340;&#21443;&#25976;
+$time = 0;#&#37325;&#26032;&#36899;&#25509;&#30340;&#27425;&#25976;
+
+#&#26368;&#21021;&#21462;&#24471;&#27284;&#26696;&#22823;&#23567;&#26041;&#27861;
+/*$lines_array = get_headers($url);
+for($i=0;$i<count($lines_array)-1;$i++){
+    similar_text($lines_array[$i],"Content-Length:",$percent);
+    if($percent>$record){
+        $record = $percent;
+        $count = $i;
+    }
+}
+$size = (int)ltrim($lines_array[$count],"Content-Length: ");*/
+
+#&#21462;&#24471;&#27284;&#26696;&#22823;&#23567;&#26041;&#27861;
+$size = get_headers($url,1)["Content-Length"];
+
+#&#23531;&#20837;Header
+header("Accept-Ranges: bytes");
+header("Content-Length: $size");
+
+
+$ch = curl_init($url);#&#24314;&#31435;curl&#36899;&#32218;
+$file = curl_exec($ch);#&#22519;&#34892;curl
+$position = (int)curl_getinfo($ch)[size_download];#&#21462;&#19979;&#36617;&#27284;&#26696;&#22823;&#23567;
+curl_close($ch);#&#38364;&#38281;&#36899;&#32218;
+
+#&#26159;&#21542;&#23436;&#25104;&#19979;&#36617;
+if($size>$position){
+    checkFinish($url,$position,$size,$time);
+}
+
+function checkFinish($url,$position,$size,$time){
+    #log&#27425;&#25976;
+    $time++;
+    $test = fopen('log','w+');
+    fwrite($test,$time);
+    fclose($test);
+    
+    $ch = curl_init($url);#&#24314;&#31435;curl&#36899;&#32218;
+    curl_setopt($ch,CURLOPT_RESUME_FROM,$position);#&#26039;&#40670;&#32396;&#20659;
+    curl_setopt($ch,CURLOPT_CONNECTTIMEOUT,0);#curl&#36899;&#32218;&#19981;&#20013;&#26039;
+    curl_setopt($ch,CURLOPT_DNS_CACHE_TIMEOUT,3600);#DNS&#26283;&#23384;1&#23567;&#26178;
+    $file = curl_exec($ch);#&#22519;&#34892;
+    $position += (int)curl_getinfo($ch)[size_download];#&#21462;&#32317;&#19979;&#36617;&#27284;&#26696;&#22823;&#23567;
+    curl_close($ch);#&#38364;&#38281;&#36899;&#32218;
+    
+    #&#30906;&#35469;&#26159;&#21542;&#23436;&#27284;
+    if($size>$position){
+        checkFinish($url,$position,$size,$time);
+    }else{
+        #log&#32080;&#26463;
+        $test = fopen('log','a+');
+        fwrite($test,'.end');
+        fclose($test);
+    }
+}
+?>
