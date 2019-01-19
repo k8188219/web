7f6136183e7d98338f37b76063b8b3d5c8ff6102
k8188219 <k8188219@mail.edu.tw>
2019-01-19 20:08:49 +0800

    Create index.php

---
+++ redirect/index.php
@@ -0,0 +1,10 @@
+<?php
+header("Location: intent://scan/#Intent".
+";scheme=".$_GET["scheme"].
+";package=".$_GET["package"].
+";action=".$_GET["package"].
+";category=".$_GET["package"].
+";component=".$_GET["package"].
+";end");
+exit();
+?>
