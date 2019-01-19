50ee63b1e659096380283ffbf9da1f46f98a479c
k8188219 <k8188219@mail.edu.tw>
2019-01-19 20:51:53 +0800

    Update index.php

--- redirect/index.php
+++ redirect/index.php
@@ -1,12 +1,13 @@
 <?php
-header("Location: intent://tw.yahoo.com#Intent;scheme=http;package=com.android.chrome;end");
-exit();
-header("Location: intent://".$_GET["intent"]."/#Intent".
+
+header("Location: intent://".$_GET["intent"]."#Intent".
 ";scheme=".$_GET["scheme"].
 ";package=".$_GET["package"].
 ";action=".$_GET["action"].
 ";category=".$_GET["category"].
 ";component=".$_GET["component"].
 ";end");
+exit();
+header("Location: intent://tw.yahoo.com#Intent;scheme=http;package=com.android.chrome;end");
 exit();
 ?>
