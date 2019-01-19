03ba95e31be7dca28936ca22ad14377c2e2c706c
k8188219 <k8188219@mail.edu.tw>
2019-01-19 20:16:00 +0800

    Update index.php

--- redirect/index.php
+++ redirect/index.php
@@ -2,9 +2,9 @@
 header("Location: intent://scan/#Intent".
 ";scheme=".$_GET["scheme"].
 ";package=".$_GET["package"].
-";action=".$_GET["package"].
-";category=".$_GET["package"].
-";component=".$_GET["package"].
+";action=".$_GET["action"].
+";category=".$_GET["category"].
+";component=".$_GET["component"].
 ";end");
 exit();
 ?>
